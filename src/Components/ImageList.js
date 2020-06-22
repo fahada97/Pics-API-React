4bc261d4289e73890c441f6be61ec057652349d3
fahada97 <syedfahad.ahmed97@gmail.com>
2020-06-22 14:28:58 -0400

    final commit, rendered in images in grid style

--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -1,11 +1,14 @@
+import "./ImageList.css";
 import React from "react";
+import ImageCard from "./ImageCard"
 
 const ImageList = (props)=>{
     const images = props.images.map((image)=>{
-        return <img src = {image.urls.regular} />
+      //const images = props.images.map(({description, id, urls}) =>{  // this is destructuring
+        return <ImageCard key ={image.id} images = {image} />
     })
     
-    return <div>{images}</div>
+    return <div className = "image-list">{images}</div>
 }
 
 export default ImageList;
